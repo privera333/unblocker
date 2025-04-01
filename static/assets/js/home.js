98de4a344187cf0af5ac624f395ae8121e068c86
privera333 <parker.rivera1@icloud.com>
2025-04-01 09:42:16 -0700

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -174,16 +174,10 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
-  "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
-  "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "parker is a fungi!",
+  "brought to you by the Phoenix Hub",
+  "kamdin wuz here!"
+  "have fun, everyone!",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
